5b94ff0e4e606f021aaae4e876a103d1b826d1e2
harrylever <ukanah15thdean@gmail.com>
2024-10-07 16:47:20 -0400

    update

---
+++ apple-website/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+
+function App() {
+  return (
+    <main className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
